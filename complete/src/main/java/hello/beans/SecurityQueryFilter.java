359ae6053d343f8467c602c06bd2962c4c464190
evan feng <live.evan@gmail.com>
2017-06-15 15:53:04 +0800

    update SecurityQueryFilter

--- complete/src/main/java/hello/beans/SecurityQueryFilter.java
+++ complete/src/main/java/hello/beans/SecurityQueryFilter.java
@@ -1,6 +1,7 @@
 package hello.beans;
 
 import leap.lang.params.Params;
+import leap.orm.sql.Sql;
 import leap.orm.sql.SqlContext;
 import leap.orm.sql.SqlTag;
 import leap.orm.sql.SqlTagProcessor;
@@ -8,7 +9,7 @@ import leap.orm.sql.SqlTagProcessor;
 public class SecurityQueryFilter implements SqlTagProcessor {
 
     @Override
-    public String processTag(SqlContext context, SqlTag tag, Params params) {
+    public String processTag(SqlContext context, Sql sql, SqlTag tag, Params params) {
         String entityName = tag.getContent();
 
         if(entityName.equals("User")) {
